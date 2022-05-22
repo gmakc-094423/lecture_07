417492bbb132137c816b555f819796319aa05f1e
gmakc-094423 <gmakc-094423@yandex.ru>
2022-05-22 11:58:47 +0300

    Лекция № 7 первые примеры

---
+++ example_01/Program.cs
@@ -0,0 +1,17 @@
+﻿// See
+
+Console.WriteLine(NumberRec(1, 10));
+
+string NumberRec(int a, int b)
+{
+if (a <= b) return $"{a} " + NumberRec(a + 1, b);
+else return String.Empty;
+}
+
+Console.WriteLine(NumberReversRec(1, 10));
+
+string NumberReversRec(int a, int b)
+{
+  if (a <= b) return NumberReversRec(a + 1, b) + $"{a} ";
+  else return String.Empty;
+}
